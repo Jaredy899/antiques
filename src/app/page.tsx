3b4462f4dc87db80d338410d270e07504eb0a823
Jared Cervantes <jared@jaredcervantes.com>
2025-04-07 16:27:54 -0400

    get rid of heroimage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import HeroImage from "~/components/HeroImage";
 import AddressLink from "~/components/AddressLink";
 import { getImagesFromFolder } from '~/utils/images';
 
